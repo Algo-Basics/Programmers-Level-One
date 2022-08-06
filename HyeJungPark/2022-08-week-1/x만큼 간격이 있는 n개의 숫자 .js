db76a5dc0441dba06d46982eb4c07e0e571db4a9
hyejj19 <hyejj19@naver.com>
2022-08-06 13:15:17 +0900

    2022-0806_문제풀이

---
+++ HyeJungPark/2022-08-week-1/x만큼 간격이 있는 n개의 숫자 .js
@@ -0,0 +1,19 @@
+// 문제
+/* 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+ */
+
+function solution(x, n) {
+  let answer = [];
+  let i = 0;
+  let num = x;
+
+  while (i < n) {
+    answer.push(num);
+    num += x;
+    i++;
+  }
+  return answer;
+}
+
+console.log(solution(2, 5));
--- HyeJungPark/2022-08-week-1/행렬의 덧셈.js
+++ HyeJungPark/2022-08-week-1/행렬의 덧셈.js
@@ -6,13 +6,24 @@
 // 입력 : 행과 열의 크기가 같은 두 행렬
 // 출력 : 같은 행, 같은 열의 값을 서로 더한 결과값 => 두 개의 배열이 포함된 배열
 // 2중 for문에서 배열의 반복 길이에 대해서 수정해보자!
+
+// 시도 1. if (arr1[i][j] && arr2[i][j]) 로 undefined 가 아닐 경우에만 코드를 실행하려고 했는데,
+// ... 없는 인덱스에 접근하다보니 프로그래머스에서 런타임 에러가 발생하는 듯 하다.폐기!
+
 function solution(arr1, arr2) {
   let answer = [[], []];
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr1.length; j++) {
-      answer[i][j] = arr1[i][j] + arr2[i][j];
+  let i = 0;
+  let j = 0;
+
+  while (i < 2) {
+    answer[i][j] = arr1[i][j] + arr2[i][j];
+    if (j < arr1.length - 1) j++;
+    else {
+      i++;
+      j = 0;
     }
   }
+
   return answer;
 }
 
